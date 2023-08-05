b82be974eaf0d579b3e10350797ad2e44c9b18d8
Code Approver <132290658+CodeApprover@users.noreply.github.com>
2023-08-05 06:22:03 +0100

    Update mirror_remote.bat

--- scripts/mirror_remote.bat
+++ scripts/mirror_remote.bat
@@ -1,64 +1,44 @@
 @echo off
 setlocal EnableDelayedExpansion
 
-REM Array of branches to ignore
+REM Array of branches to process
 set "branches=main code-development code-staging code-production"
 
 REM Move to repository directory
 pushd "%~dp0.."
 REM Switch to main, reset to remote
 git checkout main
-git sparse-checkout disable
-git reset --hard HEAD
+git reset --hard "origin/main"
 
 REM Fetch all branches from the remote repository
 git fetch --all
 
-REM Get parent directory of TEMP
-for %%i in ("%TEMP%") do set "parentDir=%%~dpi.."
-set "branchesFile=%parentDir%\branches.txt"
-
-REM Refresh the remote branches and create local branches
-git fetch --all
-git for-each-ref --format="%%(refname:short)" refs/remotes/origin | findstr /v "HEAD" | findstr /v "origin/origin" > "%branchesFile%"
-for /f "tokens=*" %%i in (%branchesFile%) do (
-    REM Strip 'origin/' from the remote branch name
-    set "localBranch=%%i"
-    set "localBranch=!localBranch:origin/=!"
-    set "createBranch=1"
-    
-    REM Check if the local branch is in the branches array
-    for %%b in (%branches%) do (
-        if "%%b"=="!localBranch!" (
-            set "createBranch=0"
-        )
-    )
-    
-    if "!createBranch!"=="1" (
-        REM Check if local branch already exists
-        git rev-parse --verify --quiet "!localBranch!" >nul 2>&1
-        if errorlevel 1 (
-            REM Create and checkout a new local branch that tracks the remote branch
-            git checkout -b "!localBranch!" "%%i"
-            REM Push the local branch to the remote repository
-            git push -u origin "!localBranch!"
-        )
-    )
-)
-
-REM Delete specific local branches defined in "branches" except main
+REM Process the specific branches
 for %%b in (%branches%) do (
-    if "%%b" neq "main" (
-        git branch -D %%b > nul 2>&1
+    REM Check if remote branch exists
+    git ls-remote --exit-code --heads origin %%b >nul 2>&1
+    if errorlevel 1 (
+        REM If remote branch does not exist, delete local forcefully
+        git branch -D %%b >nul 2>&1
+    ) else (
+        REM Check if local branch already exists
+        git rev-parse --verify --quiet "%%b" >nul 2>&1
+        if errorlevel 1 (
+            REM If not, create and checkout a new local branch that tracks the remote branch
+            git checkout -b "%%b" "origin/%%b"
+        ) else (
+            REM If exists, check out and reset to match remote
+            git checkout "%%b"
+            git reset --hard "origin/%%b"
+        )
     )
 )
 
-REM Switch to main and report
+REM Switch to main
 git checkout main
-git fetch --all
-git pull
-git push
-git branch
+
+REM Report
+git branch -a
 
 REM Return to the original directory
 popd
