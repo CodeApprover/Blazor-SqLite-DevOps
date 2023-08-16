699969a1472b393b299aeabae24bae484b8e8515
code-backups <404bot@pm.me>
2023-08-16 12:20:21 +0100

    [skip ci]

--- execute-workflow.sh
+++ execute-workflow.sh
@@ -3,17 +3,6 @@
 # Disable command echo
 set +x
 
-# Commit and push to test workflows [skip ci]
-echo "0" >> workflow.driver
-git add workflow.driver
-git commit -m "running dev push to test workflows #0 [skip ci]"
-git push
-
-# Refresh local branch
-git pull
-ls -la
-sleep 30
-
 # Commit and push to test workflows
 echo "0" >> workflow.driver
 git add workflow.driver
