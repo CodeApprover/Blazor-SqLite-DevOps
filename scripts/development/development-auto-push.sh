8c88ceec6ab14b722a8dad0e369a4dcea16f8c3b
CodeApprover <pucfada@pm.me>
2023-09-04 20:54:43 +0100

    updating scripts

--- scripts/development/development-auto-push.sh
+++ scripts/development/development-auto-push.sh
@@ -3,6 +3,14 @@
 set -e  # Exit on any command failure
 set -x  # Print all commands
 
+# Branch Information
+TARGET_BRANCH="code-development"
+PROJ_NAME="Blazor-SqLite-DevOps"
+
+# Constants
+NUM_COMMITS=3
+WAIT_DURATION=120 # seconds
+
 # Check if the script is being run from the correct directory
 CURRENT_DIR=$(basename "$(pwd)")
 if [[ "$CURRENT_DIR" != "scripts" ]]; then
@@ -12,29 +20,46 @@ fi
 
 function update_workflow_driver() {
     {
-      echo "Push iteration: $1 of $2"
-      echo "Branch: code-development"
-      echo "Username: Code-Backups"
-      echo "Email: 404bot@pm.me"
-      echo "Automated push identifying push $1 of $2 iterations."
-      cat bot.ascii
-    } > "../development/$PROJ_NAME/workflow.driver"
+        echo "Push iteration: $1 of $2"
+        echo "Branch: $TARGET_BRANCH"
+        echo "Username: $USER_NAME"
+        echo "Email: $USER_EMAIL"
+        echo "Automated push identifying push $1 of $2 iterations."
+        cat bot.ascii
+    } > "../$TARGET_BRANCH/$PROJ_NAME/workflow.driver"
 }
 
-# Constants
-NUM_COMMITS=3
-WAIT_DURATION=120 # seconds
-MAIN_USER="Code-Backups"
-MAIN_EMAIL="404bot@pm.me"
-PROJ_NAME="Blazor-SqLite-DevOps"
+# Determine user based on the selected branch
+case "$TARGET_BRANCH" in
+    main)
+        USER_NAME="CodeApprover"
+        USER_EMAIL="pucfada@pm.me"
+        ;;
+    code-development)
+        USER_NAME="Code-Backups"
+        USER_EMAIL="404bot@pm.me"
+        ;;
+    code-staging)
+        USER_NAME="ScriptShifters"
+        USER_EMAIL="lodgings@pm.me"
+        ;;
+    code-production)
+        USER_NAME="CodeApprover"
+        USER_EMAIL="pucfada@pm.me"
+        ;;
+    *)
+        echo "Invalid branch: $TARGET_BRANCH"
+        exit 1
+        ;;
+esac
 
 # WARNING message
 echo "CAUTION:"
 echo "This script will perform the following operations:"
 echo "Commits workflow.driver files"
-echo "to the 'code-development' branch development directory"
+echo "to the '$TARGET_BRANCH' branch development directory"
 echo "$NUM_COMMITS times every $WAIT_DURATION seconds."
-echo "Three workflow.driver files will be committed to the 'code-development' branch."
+echo "Three workflow.driver files will be committed to the '$TARGET_BRANCH' branch."
 echo ""
 read -p "Do you wish to proceed? (y/n): " -r
 
@@ -45,22 +70,22 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 fi
 
 # Inform user about the operations
-echo "Updating file for branch: code-development"
+echo "Updating file for branch: $TARGET_BRANCH"
 
 # Configure git
-git config user.name "$MAIN_USER"
-git config user.email "$MAIN_EMAIL"
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
 
 # Checkout the branch
 git fetch --all
-git checkout "code-development"
+git checkout "$TARGET_BRANCH"
 git pull
 
 # Committing and pushing in a loop
 for i in $(seq 1 $NUM_COMMITS); do
     update_workflow_driver "$i" "$NUM_COMMITS"
-    git add "../development/$PROJ_NAME/workflow.driver"
-    git commit -m "Automated code-development push by $MAIN_USER #$i of $NUM_COMMITS"
+    git add "../$TARGET_BRANCH/$PROJ_NAME/workflow.driver"
+    git commit -m "Automated $TARGET_BRANCH push by $USER_NAME #$i of $NUM_COMMITS"
     git push
     sleep $WAIT_DURATION
 done
