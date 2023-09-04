2cf7db47b5ef9398207ebe6692116e473e376d31
code-backups <404bot@pm.me>
2023-09-04 19:42:06 +0100

    updating scripts

--- scripts/.devops/reset-all-branches-to-main.sh
+++ scripts/.devops/reset-all-branches-to-main.sh
@@ -1,221 +1,201 @@
 #!/bin/bash
 
-set -e  # Exit if a command fails.
-set -x # Print commands for debugging.
+set -e  # Exit if any command fails.
+set -x  # Print commands for debugging.
+
+# Exit Codes:
+# 1: Incorrect script execution directory.
+# 2: Incorrect number of iterations.
+# 3: Incorrect wait duration.
+# 4: Incorrect usage or missing argument.
+# 5: Invalid branch name as argument.
+# 6: User cancelled the operation after warning message.
+# 7: User cancelled during directory selection for the main branch.
+
+# Constants
+DEFAULT_NUM_COMMITS=3
+DEFAULT_WAIT_DURATION=45  # seconds
+MAIN_USER="CodeApprover"
+MAIN_EMAIL="pucfada@pm.me"
+PROJ_NAME="Blazor-SqLite-Golf-Club"
+CURRENT_DIR=$(pwd)
+BRANCHES=("main" "code-development" "code-staging" "code-production")
+
+# Number of iterations
+NUM_COMMITS=${2:-$DEFAULT_NUM_COMMITS}
+if ! [[ "$NUM_COMMITS" =~ ^[0-9]+$ ]]; then
+    echo "Error: The number of iterations must be an integer."
+    exit 2
+fi
+
+# Wait duration
+WAIT_DURATION=${3:-$DEFAULT_WAIT_DURATION}
+if ! [[ "$WAIT_DURATION" =~ ^[0-9]+$ ]]; then
+    echo "Error: The wait duration must be an integer."
+    exit 3
+fi
 
 # Set caveat.
 WARNING_MESSAGE=$(cat << EOM
 
 CAUTION:
 
-This script automates the recreation of
-code-development, code-staging and code-production branches
-using main as the source.
-
-The script assumes that required users have the necessary permissions.
+This script automates pushing workflow.driver changes to a specific branch
+and assumes that required users have the necessary permissions.
 
 It performs the following tasks:
 
-    1. Checkout and update of the main branch.
-    2. DELETE all local and remote branches except 'main'.
-    3. Create and setup fresh branches named 'code-development', 'code-staging' and 'code-production' based on 'main'.
-    4. Ensure that each code- branch only has its specific required directories and files.
-    5. Returns to the main branch.
+    1. Verifies the directory from which it is run.
+    2. Accepts a branch name as an argument.
+    3. Updates the 'workflow.driver' file with predefined content.
+    4. Commits the updated file to the specified branch multiple times at a defined interval.
+    5. Offers branch-specific directory selection for the 'main' branch.
+    6. Restores the git environment after execution.
 
 Consequences:
 
-    1. Any unmerged changes in deleted branches will be LOST FOREVER.
-    2. The deleted branches on the remote could impact other collaborators' work.
-    3. The operations are irreversible. Ensure you have a backup if needed.
-    4. All stashes will be dropped and lost.
-
-Exit Codes:
-
-    0. Script executed successfully without errors.
-    1. User chose to exit without making changes.
-    2. Run script from the correct directory.
-    3. Production directory not found in the parent directory.
-    4. Staging directory not found in the parent directory.
-    5. Scripts directory not found in the parent directory.
-    6. Development directory not found in the specified search path.
+    1. The script will automate git operations that may affect the repository's branches and files.
+    2. Incorrect usage or misconfiguration can lead to unexpected changes and loss of data.
+    3. Use with caution and ensure you have backup copies of important files.
 
 EOM
 )
 
-# Issue warning.
 echo "$WARNING_MESSAGE"
 
-# Check user response.
-echo && read -p "Do you wish to proceed? (y/n): " -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    echo "Exiting without making changes."
-    exit 1
-fi
-
-# Normalise path separators based on operating system.
-OS=$(uname)
-CURRENT_DIR=$(pwd)
-if [[ "$OS" != "Linux" ]]; then # Windows/MinGW.
-    CURRENT_DIR=${CURRENT_DIR//\\/\/}
-fi
-
-# Check if script is run from correct directory.
 CURRENT_DIR=$(pwd)
 EXPECTED_DIR="scripts/.devops"
 if [[ "$CURRENT_DIR" != *"$EXPECTED_DIR" ]]; then
     echo "Error: Please run this script from within its own directory ($EXPECTED_DIR/)."
-    exit 2
+    exit 1
 fi
 
-# Configure git user.
-git config user.name "CodeApprover" --local
-git config user.email "pucfada@pm.me" --local
+# Check if no argument is provided for the branch name
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 <branch> [number_of_iterations] [wait_duration]"
+    echo "e.g., $0 code-development 5 60"
+    echo "Available options for <branch>: ${BRANCHES[*]}"
+    exit 4
+fi
 
-# Navigate to repository root
-cd "$CURRENT_DIR/../../"
+branch=$1
+if [[ ! " ${BRANCHES[*]} " =~ $branch ]]; then
+    echo "Invalid branch: $branch. Available branches are: ${BRANCHES[*]}"
+    exit 5
+fi
 
-# Checkout and update the main branch.
-git checkout main
-git stash # Prevents uncommitted changes error.
-git fetch --all --tags --prune
-git pull
+echo && read -p "Do you wish to proceed? (y/n): " -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Exiting without making changes."
+    exit 6
+fi
 
-# Set branch names.
-branches=("code-development" "code-staging" "code-production")
+# Stash uncommitted changes in the current branch.
+if [[ $(git status --porcelain) ]]; then
+    echo "Stashing uncommitted changes in current branch..."
+    git stash
+    CURRENT_BRANCH_STASHED=true
+fi
 
-# Delete local branches.
-for branch in "${branches[@]}"; do
-    if git show-ref --quiet "refs/heads/$branch"; then
-        git branch -D "$branch"
-    fi
+# Stash changes for the target branch.
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git checkout "$1"
+if [[ $(git status --porcelain) ]]; then
+    echo "Stashing uncommitted changes on branch $1..."
+    git stash
+    TARGET_BRANCH_STASHED=true
+fi
+git checkout "$CURRENT_BRANCH"
+
+# Extract MAIN_DIRS from BRANCHES and set the FILE_PATH.
+for branch_item in "${BRANCHES[@]}"; do
+    case "$branch_item" in
+        main)
+            MAIN_DIRS=("development" "staging" "production")
+        ;;
+        code-*)
+            MAIN_DIRS=("${branch_item#code-}")
+        ;;
+    esac
 done
 
-# Delete remote branches.
-for remote_branch in "${branches[@]}"; do
-    if git show-ref --quiet "refs/remotes/origin/$remote_branch"; then
-        git push origin --delete "$remote_branch"
-    fi
+case "$branch" in
+    main)
+        PS3='Which directory do you want to update in main? (or 4 to cancel): '
+        select dir in "${MAIN_DIRS[@]}" "cancel"; do
+            case $dir in
+                development|staging|production)
+                    break
+                ;;
+                cancel)
+                    echo "User cancelled. Exiting."
+                    exit 7
+                ;;
+            esac
+        done
+        FILE_PATH="$CURRENT_DIR/../../$dir/$PROJ_NAME/workflow.driver"
+        USER_NAME=$MAIN_USER
+        USER_EMAIL=$MAIN_EMAIL
+    ;;
+    code-*)
+        dir="${branch#code-}"
+        FILE_PATH="$CURRENT_DIR/../../$dir/$PROJ_NAME/workflow.driver"
+        USER_NAME=$MAIN_USER
+        USER_EMAIL=$MAIN_EMAIL
+    ;;
+esac
+
+# Inform user about the operations.
+echo "Updating file: $FILE_PATH for branch: $branch"
+
+# Configure git
+git config --replace-all user.name "$USER_NAME"
+git config --replace-all user.email "$USER_EMAIL"
+
+# Checkout the branch
+git fetch --all
+git checkout "$branch"
+git pull
+
+function update_workflow_driver() {
+    {
+        echo "Push iteration: $1 of $2 to branch: $branch by:"
+        echo "Username: $USER_NAME Email: $USER_EMAIL"
+        echo "Automated push $1 of $2"
+        echo ""
+    } > "$FILE_PATH"
+}
+
+# Commit and push in a loop.
+for i in $(seq 1 "$NUM_COMMITS"); do
+    update_workflow_driver "$i" "$NUM_COMMITS"
+    git add "$FILE_PATH"
+    git commit -m "Running $branch push #$i"
+    git push
+
+    # Countdown timer
+    echo "Waiting for the next push..."
+    for j in $(seq "$WAIT_DURATION" -1 1); do
+        echo -ne "$j seconds remaining...\r"
+        sleep 1
+    done
+    echo
 done
 
-# Find the development directory.
-development=$(find "$CURRENT_DIR/../../" -type d -name "development" | head -n 1)
+# Return to main and reset user and dir.
+git config --replace-all user.name "$MAIN_USER"
+git config --replace-all user.email "$MAIN_EMAIL"
+git checkout main
+cd "$CURRENT_DIR"
 
-# If the development directory is found.
-if [[ -n "$development" ]]; then
-    # Extract parent directory of development directory.
-    parent_dir=$(dirname "$development")
-
-    # Use parent directory to locate the other directories.
-    production="$parent_dir/production"
-    staging="$parent_dir/staging"
-    scripts="$parent_dir/scripts"
-
-    # Check if required directories exist.
-    ! [[ -d "$production" ]] && echo "Production directory not found at: $production" && exit 3
-    ! [[ -d "$staging" ]] && echo "Staging directory not found at: $staging" && exit 4
-    ! [[ -d "$scripts" ]] && echo "Scripts directory not found at: $scripts" && exit 5
-else
-    ls -la "$CURRENT_DIR/../../"
-    echo "Development directory not found." && exit 6
+# Pop the stash for the target branch.
+if [[ $TARGET_BRANCH_STASHED == true ]]; then
+    git checkout "$1"
+    git stash pop
+    git checkout "$CURRENT_BRANCH"
 fi
 
-# Function to git remove files or dirs safely.
-safe_git_rm() {
-    local path="$1"
-    if [[ -d "$path" ]]; then
-        git rm -r "$path"
-    else
-        git rm "$path"
-    fi
-}
-
-# Function to process the scripts directory for each branch.
-process_scripts_dir() {
-    local branch="$1"
-    subdir=${branch//code-/}
-
-    # Check if the specific subdir exists.
-    if [[ ! -d "$scripts/$subdir" ]]; then
-        echo "Error: $subdir directory not found in $scripts. Exiting..."
-        exit 7  # Using exit code 7 to indicate this specific error.
-    fi
-
-    # Copy the subdir directly to the project root as 'toolbox'.
-    cp -r "$scripts/$subdir" "toolbox"
-
-    # Remove the entire scripts directory from git and the file system.
-    git rm -r "$scripts"
-
-    # Ensure the directory is removed from the file system.
-    if [[ -d "$scripts" ]]; then
-        rm -rf "$scripts"
-    fi
-
-    # Add the new scripts directory to git.
-    cd "toolbox"
-    git add -A
-    cd -  # Navigate back to the original directory to ensure the rest of the script runs correctly.
-}
-
-# Set up code-development dirs.
-git checkout -b code-development main
-safe_git_rm "$production"
-safe_git_rm "$staging"
-process_scripts_dir "code-development"
-
-# Add all changes,
-git add .
-git commit -m "Setup new code-development branch. [skip ci]"
-git push -u --set-upstream origin code-development
-git reset --hard HEAD
-git checkout main
-
-# Set up code-staging dirs.
-git checkout -b code-staging main
-safe_git_rm "$production"
-process_scripts_dir "code-staging" "staging"
-
- # Add all changes,
-git add .
-git commit -m "Setup new code-staging branch. [skip ci]"
-git push -u --set-upstream origin code-staging
-git reset --hard HEAD
-git checkout main
-
-# Set up code-production dirs.
-git checkout -b code-production main
-safe_git_rm "$development"
-process_scripts_dir "code-production" "production"
-
-# Add all changes,
-git add .
-git commit -m "Setup new code-production branch. [skip ci]"
-git push -u --set-upstream origin code-production
-git reset --hard HEAD
-git checkout main
-
-# Switch back to main branch.
-git checkout main
-git stash clear
-git fetch --all --tags --prune
-git pull
-git status
-git branch -a
-
-###### CLEANUP ######
-
-# Set line ending conversion tool.
-LINE_ENDING_TOOL="unix2dos"
-if [[ "$OS" == "Linux" ]]; then
-    LINE_ENDING_TOOL="dos2unix"
+# Pop the stash for the current branch.
+if [[ $CURRENT_BRANCH_STASHED == true ]]; then
+    git stash pop
 fi
-
-# Process line ending conversion.
-find "$CURRENT_DIR" -type f -exec "$LINE_ENDING_TOOL" {} \;
-
-# Remove trailing spaces from all lines.
-find "$CURRENT_DIR" -type f -exec sed -i 's/[[:space:]]*$//' {} \;
-
-# Replace multiple consecutive empty lines with one.
-find "$CURRENT_DIR" -type f -exec sed -i '/^$/N;/^\n$/D' {} \;
