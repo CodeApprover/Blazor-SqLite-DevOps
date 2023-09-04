bfa6fdd85011fca77e0a1265b45940f293149af8
CodeApprover <pucfada@pm.me>
2023-09-04 23:52:37 +0100

    updating scripts

--- scripts/development/development-auto-push.sh
+++ scripts/development/development-auto-push.sh
@@ -1,59 +1,68 @@
 #!/bin/bash
 
-set -e  # Exit on any command failure.
-set -x  # Print all commands.
+set -e # Exit nonzero if anything fails.
+set -x # Echo all commands.
 
-# Constants
+# Set constants.
+BRANCH="code-development"
 PROJ_NAME="Blazor-SqLite-Golf-Club"
 
-# Remember the current branch to revert to it later.
-CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-# Constants with default values.
-DEFAULT_BRANCH="code-development"
+# Set defaults.
 NUM_COMMITS_DEFAULT=1
-WAIT_DURATION_DEFAULT=100 # seconds.
+WAIT_DURATION_DEFAULT=100
+DEFAULT_COMMIT_MSG="Automated $BRANCH push by $USER_NAME"
+DEFAULT_EXTRA_MSG=""
 
-# Check the number of parameters passed and assign values accordingly.
+# Parse the number of command line arguments and assign values accordingly.
 case $# in
     0)
-        # Use default values.
-        TARGET_BRANCH=$DEFAULT_BRANCH
         NUM_COMMITS=$NUM_COMMITS_DEFAULT
         WAIT_DURATION=$WAIT_DURATION_DEFAULT
+        COMMIT_MSG=$DEFAULT_COMMIT_MSG
+        EXTRA_MSG=$DEFAULT_EXTRA_MSG
     ;;
     1)
-        TARGET_BRANCH=$DEFAULT_BRANCH
         NUM_COMMITS=$1
         WAIT_DURATION=$WAIT_DURATION_DEFAULT
+        COMMIT_MSG=$DEFAULT_COMMIT_MSG
+        EXTRA_MSG=$DEFAULT_EXTRA_MSG
     ;;
     2)
-        TARGET_BRANCH=$DEFAULT_BRANCH
         NUM_COMMITS=$1
         WAIT_DURATION=$2
+        COMMIT_MSG=$DEFAULT_COMMIT_MSG
+        EXTRA_MSG=$DEFAULT_EXTRA_MSG
+    ;;
+    3)
+        NUM_COMMITS=$1
+        WAIT_DURATION=$2
+        COMMIT_MSG=$3
+        EXTRA_MSG=$DEFAULT_EXTRA_MSG
+    ;;
+    4)
+        NUM_COMMITS=$1
+        WAIT_DURATION=$2
+        COMMIT_MSG=$3
+        EXTRA_MSG=$4
     ;;
     *)
-        echo "Error: Invalid number of arguments. Expected 0, 1, or 2 arguments, but got $#."
+        # Provide usage instructions
+        echo "Error: Invalid number of arguments. Expected 0 to 4 arguments, but got $#."
         echo "Usage:"
-        echo "$0                       # Use default values"
-        echo "$0 [num_commits]        # Specify number of commits"
-        echo "$0 [num_commits] [wait_duration]   # Specify number of commits and wait duration in seconds"
+        echo "$0 [num_commits] [wait_duration] [commit_msg] [extra_msg]"
         exit 1
     ;;
 esac
 
-# Set branch information.
-BRANCH="${TARGET_BRANCH//code-}"
-
-# Check if the script is being run from the correct directory
+# Check the directory from which the script is being run.
 EXPECTED_DIR="scripts/$BRANCH"
 if [[ ! "$PWD" =~ $EXPECTED_DIR ]]; then
-    echo "Error: Please run this script from within its own directory ($EXPECTED_DIR)."
-    exit 1
+    echo "Error: Please run this script from its directory ($EXPECTED_DIR)."
+    exit 2
 fi
 
-# Determine user based on the selected branch.
-case "$TARGET_BRANCH" in
+# Assign Git username and email based on branch.
+case "$BRANCH" in
     main)
         USER_NAME="CodeApprover"
         USER_EMAIL="pucfada@pm.me"
@@ -71,67 +80,54 @@ case "$TARGET_BRANCH" in
         USER_EMAIL="pucfada@pm.me"
     ;;
     *)
-        echo "Invalid branch: $TARGET_BRANCH"
-        exit 1
+        echo "Invalid branch: $BRANCH"
+        exit 3
     ;;
 esac
 
-# WARNING message.
-echo "CAUTION:"
-echo "This script will perform the following operations:"
-echo "Commits workflow.driver files"
-echo "to the '$TARGET_BRANCH' branch development directory"
-echo "$NUM_COMMITS times every $WAIT_DURATION seconds."
-echo "Three workflow.driver files will be committed to the '$TARGET_BRANCH' branch."
-echo ""
+# Warning before executing the script.
+echo "WARNING:"
+echo "This script will:"
+echo "- Commit workflow.driver files to the '$BRANCH' branch development directory."
+echo "- Make $NUM_COMMITS commits at intervals of $WAIT_DURATION seconds."
+echo "Please ensure you are aware of the changes this script will make."
 read -p "Do you wish to proceed? (y/n): " -r
 
-# Check for the user's response.
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
     echo "Exiting without making changes."
-    exit 2
+    exit 4
 fi
 
-# Inform user about the operations.
-echo "Updating file for branch: $TARGET_BRANCH"
-
-# Configure git globally.
+# Fetch latest changes and switch to the target branch.
 git config --global user.name "$USER_NAME"
 git config --global user.email "$USER_EMAIL"
-
-# Checkout the branch.
-git fetch --all
-git checkout "$TARGET_BRANCH"
+git fetch --all --tags --prune --prune-tags
+git checkout "$BRANCH"
+git stash
 git pull
 
-# Committing and pushing in a loop.
+# Commit workflow.driver in a loop.
 for i in $(seq 1 "$NUM_COMMITS"); do
     {
-        echo "Push iteration: $i of $NUM_COMMITS every $WAIT_DURATION seconds."
-        echo "Branch: $TARGET_BRANCH"
+        echo "Commit iteration: $i of $NUM_COMMITS (every $WAIT_DURATION seconds)."
+        echo "Branch: $BRANCH"
         echo "Username: $USER_NAME"
         echo "Email: $USER_EMAIL"
         echo "Date: $(date)"
+        echo "$EXTRA_MSG"
     } >> "../../development/$PROJ_NAME/workflow.driver"
 
     git add "../../development/$PROJ_NAME/workflow.driver"
-    git commit -m "Automated $TARGET_BRANCH push by $USER_NAME #$i of $NUM_COMMITS every $WAIT_DURATION seconds."
+    git commit -m "$COMMIT_MSG"
     git push
 
-    # Countdown timer
     if [ "$i" -lt "$NUM_COMMITS" ]; then
-        echo "Waiting for the next push..."
-        for j in $(seq "$WAIT_DURATION" -1 1); do
-            echo -ne "$j seconds remaining...\r"
-            sleep 1
-        done
-        echo
+        echo "Waiting for the next commit..."
+        sleep "$WAIT_DURATION"
     fi
 done
 
-# Return to the original branch and reset user.
-git config --global user.name "CodeApprover"
-git config --global user.email "pucfada@pm.me"
-git checkout "$CURRENT_BRANCH"
-git fetch --all
+# Restore stashed changes and fetch latest changes.
+git stash pop
+git fetch --all --tags --prune --prune-tags
 git pull
