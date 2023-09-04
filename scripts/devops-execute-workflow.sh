9a8416927d40f4428ebbc4b364897e0375ab5171
CodeApprover <pucfada@pm.me>
2023-09-04 10:52:53 +0100

    fixing bash driver scripts

--- scripts/devops-execute-workflow.sh
+++ scripts/devops-execute-workflow.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-set -e  # Exit on any command failure
+set -e  # Exit if any command fails
+set -x  # Print all commands for debugging
+
+function update_workflow_driver() {
+    {
+        echo "Push iteration: $1 of $2 to branch: $branch by:"
+        echo "Username: $USER_NAME Email: $USER_EMAIL"
+        echo "Automated push $1 of $2"
+        echo ""
+        cat bot.ascii
+    } > "$FILE_PATH"
+}
 
 # Constants
 NUM_COMMITS=3
@@ -9,39 +20,56 @@ MAIN_USER="CodeApprover"
 MAIN_EMAIL="pucfada@pm.me"
 PROJ_NAME="Blazor-SqLite-DevOps"
 
-# Array of available branches
+# Available branches
 BRANCHES=("main" "code-development" "code-staging" "code-production")
 MAIN_DIRS=("development" "staging" "production") # Directories in 'main' branch
 
-# Validate the parameter
-if [ "$#" -ne 1 ]; then
+# Check if no argument is provided
+if [ $# -ne 1 ]; then
     echo "Usage: $0 <branch>"
+    echo "Available options: ${BRANCHES[*]}"
     exit 1
 fi
 
-branch="$1"
+branch=$1
 
-# Validate branch
+# Check if the provided branch is valid
 if [[ ! " ${BRANCHES[*]} " =~ $branch ]]; then
     echo "Invalid branch: $branch. Available branches are: ${BRANCHES[*]}"
     exit 2
+fi
+
+# WARNING message
+echo "CAUTION:"
+echo "This script will perform the following operations:"
+echo "1. Commits workflow.driver files to"
+echo "   the '$branch' branch ${branch#code-} directory"
+echo "   $NUM_COMMITS times every $WAIT_DURATION seconds."
+echo "- Three workflow.driver files will be committed to the '$branch' branch."
+echo ""
+read -p "Do you wish to proceed? (y/n): " -r
+
+# Check user response
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Exiting without making changes."
+    exit 3
 fi
 
 # Set directory and user info based on branch
 case "$branch" in
     main)
         # If running main choose a directory
-        while true; do
-            echo "Available directories in main: ${MAIN_DIRS[*]}"
-            read -r -p "Which directory do you want to update in main? (or 'cancel' to exit) " dir
-            if [[ " ${MAIN_DIRS[*]} " =~ $dir ]]; then
-                break
-                elif [[ "$dir" == "cancel" ]]; then
-                echo "User canceled. Exiting."
-                exit 4
-            else
-                echo "Invalid directory. Try again or enter 'cancel' to exit."
-            fi
+        PS3='Which directory do you want to update in main? (or 4 to cancel): '
+        select dir in "${MAIN_DIRS[@]}" "cancel"; do
+            case $dir in
+                development|staging|production)
+                    break
+                ;;
+                cancel)
+                    echo "User canceled. Exiting."
+                    exit 4
+                ;;
+            esac
         done
         FILE_PATH="$dir/$PROJ_NAME/workflow.driver"
         USER_NAME=$MAIN_USER
@@ -78,7 +106,7 @@ git pull
 
 # Committing and pushing in a loop
 for i in $(seq 1 $NUM_COMMITS); do
-    echo "Environment updating the workflow driver file - push #$i" >> "$FILE_PATH"
+    update_workflow_driver "$i" "$NUM_COMMITS"
     git add "$FILE_PATH"
     git commit -m "Running $branch push #$i"
     git push
