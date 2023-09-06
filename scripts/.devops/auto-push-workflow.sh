c3c43c9cea63d0596c9ad65046cab72fb66caa6d
Code Approver <132290658+CodeApprover@users.noreply.github.com>
2023-09-06 17:09:54 +0100

    Update auto-push-workflow.sh[skip ci]

--- scripts/.devops/auto-push-workflow.sh
+++ scripts/.devops/auto-push-workflow.sh
@@ -1,221 +1,185 @@
 #!/bin/bash
 
-set -e  # Exit if any command fails.
-#set -x # Print commands for debugging.
+set -e  # Exit for any error
+#set -x # Display commands
+
+# Exits
+SUCCESS=0
+USER_ABORT=21
+USAGE_ERR=22
+INV_BRANCH=23
+ITER_ERR=24
+WAIT_ERR=25
+CSPROJ_ERR=26
+
 
 # Constants
-NUM_COMMITS_DEFAULT=1
-WAIT_DURATION_DEFAULT=0 # seconds
-MAIN_USER="CodeApprover"
-MAIN_EMAIL="pucfada@pm.me"
+DEV_USER="CodeApprover"
+DEV_EMAIL="pucfada@pm.me"
 PROJ_NAME="Blazor-SqLite-Golf-Club"
-ERROR_USAGE=2
-ERROR_INVALID_BRANCH=3
+CUR_DIR=$(realpath "$(pwd)")
+PREFIX="code-"
+BRANCHES=("main" "${PREFIX}development" "${PREFIX}staging" "${PREFIX}production")
+MAX_WAIT=86400  # 24 hours
+MAX_PUSHES=5
 
-BRANCHES=("main" "code-development" "code-staging" "code-production")
+# Set usage message
+USAGE="Usage: $0 <branch> [<num_pushes> <wait_duration>]"
 
-# User information for different branches
-USER_INFO=(
-    ["code-development"]="Code-Backups 404bot@pm.me"
-    ["code-staging"]="ScriptShifters lodgings@pm.me"
-    ["code-production"]="CodeApprover pucfada@pm.me"
-)
+# Warn user
+cat <<EOM
 
-# Set caveat.
-WARNING_MESSAGE=$(cat << EOM
+WARNING:
+
+- You are about to execute $0
+
+This script makes commits and pushes to a specified branch
+for a specific number of iterations
+waiting for a specific duration between pushes.
+
+USERS INVOLVED:
+
+- Main: $DEV_USER (Email: $DEV_EMAIL)
+- Development: ${USER_INFO["${PREFIX}development"]}
+- Staging: ${USER_INFO["${PREFIX}staging"]}
+- Production: ${USER_INFO["${PREFIX}production"]}
+
 CAUTION:
 
-This script assumes that required users have the necessary permissions.
+The script stashes and pops any stashes (if created)
+to restore any changes in the current branch.
+Consoder making a repo backup before running this script.
 
-The script automates pushing workflow.driver changes
-to a specific branch.
+$USAGE
 
-It performs the following tasks:
-    1. Verifies the directory from which it is run.
-    2. Accepts a branch name as an argument.
-    3. Updates the 'workflow.driver' file with predefined content.
-    4. Commits the updated file to the specified branch multiple times at a defined interval.
-    5. Offers branch-specific directory selection for the 'main' branch.
-    6. Restores the git environment after execution.
-
-Consequences:
-    1. The script will automate git operations that may affect the repository's branches and files.
-    2. Incorrect usage or misconfiguration can lead to unexpected changes and loss of data.
-    3. Use with caution and ensure you have backup copies of important files.
-
-Exit Codes:
-    0. Script executed successfully without errors.
-    1. Incorrect script execution directory.
-    2. Incorrect usage or missing argument.
-    3. Invalid branch name as argument.
-    4. User cancelled the operation after warning message.
-    5. User cancelled during directory selection for the main branch.
 EOM
+
+# Parse user response
+read -r -p "Continue? [yes/no] " response
+responses=("y" "Y" "yes" "YES" "Yes")
+[[ ! "${responses[*]}" =~ $response ]] && echo "Aborted." && exit "$USER_ABORT"
+
+# Set local variables
+branch="$1"
+num_pushes="${2:-1}"     # default 1
+wait_duration="${3:-0}"  # default 0
+
+# Validate variables
+[[ $# -lt 1 || $# -gt 3 || ! "$branch" =~ ^[a-zA-Z]+$ || ! "$num_pushes" =~ ^[0-9]+$ || ! "$wait_duration" =~ ^[0-9]+$ ]] && echo "$USAGE" && echo "Invalid params." && exit "$USAGE_ERR"
+[[ ! " ${BRANCHES[*]} " =~ $branch ]] && echo "$USAGE" && echo "Invalid branch." && exit "$INV_BRANCH"
+[[ "$num_pushes" -lt 1 || "$num_pushes" -gt "$MAX_PUSHES" ]] && echo "$USAGE" && echo "Invalid iteration count." && exit "$ITER_ERR"
+[[ "$wait_duration" -lt 0 || "$wait_duration" -gt "$MAX_WAIT" ]] && echo "$USAGE" && echo "Invalid wait duration." && exit "$WAIT_ERR"
+
+# Set user
+declare -A USER_INFO
+USER_INFO=(
+  ["${BRANCHES[0]}"]="$DEV_USER $DEV_EMAIL"
+  ["${BRANCHES[1]}"]="Code-Backups 404bot@pm.me"
+  ["${BRANCHES[2]}"]="ScriptShifters lodgings@pm.me"
+  ["${BRANCHES[3]}"]="$DEV_USER $DEV_EMAIL"
 )
+USER=${USER_INFO["$branch"]}
+USER_NAME=${USER%% *}
+USER_EMAIL=${USER#* }
 
-# Disaplay caveat.
-echo "$WARNING_MESSAGE"
+# Set environment and csproj file
+env="${branch#code-}"
+CSPROJ="$CUR_DIR/../../$env/$PROJ_NAME/$PROJ_NAME.csproj"
+[ ! -f "$CSPROJ" ] && echo "No file at $CSPROJ." && exit "$CSPROJ_ERR"
 
-# Verify script execution directory
-CURRENT_DIR=$(pwd)
-EXPECTED_DIR="scripts/.devops"
-if [[ "$CURRENT_DIR" != *"$EXPECTED_DIR" ]]; then
-    echo "Error: Please run this script from within its own directory ($EXPECTED_DIR/)."
-    exit 1
-fi
+# Set git login
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
 
-# Check and parse command-line arguments
-if [ $# -lt 1 ] || [ $# -gt 3 ]; then
-    echo && echo "Usage: $0 <branch> [number_of_iterations] [wait_duration]"
-    echo "Available options for <branch>: ${BRANCHES[*]}"
-    exit $ERROR_USAGE
-fi
-
-# Verify the branch name provided as the first argument
-branch=$1
-if [[ ! " ${BRANCHES[*]} " =~ $branch ]]; then
-    echo "Invalid branch: $branch. Available branches are: ${BRANCHES[*]}"
-    exit $ERROR_INVALID_BRANCH
-fi
-
-# Parse the remaining command-line arguments
-num_commits=$NUM_COMMITS_DEFAULT
-wait_duration=$WAIT_DURATION_DEFAULT
-
-if [ $# -ge 2 ]; then
-    if ! [[ $2 =~ ^[0-9]+$ ]]; then
-        echo "Invalid number of iterations: $2. Must be a positive integer."
-        exit $ERROR_USAGE
-    fi
-    num_commits=$2
-fi
-
-if [ $# -ge 3 ]; then
-    if ! [[ $3 =~ ^[0-9]+$ ]]; then
-        echo "Invalid wait duration: $3. Must be a positive integer."
-        exit $ERROR_USAGE
-    fi
-    wait_duration=$3
-fi
-
-# Ask user for confirmation
-echo && read -p "Do you wish to proceed? (y/n): " -r
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-    echo "Exiting without making changes."
-    exit 4
-fi
-
-# Stash uncommitted changes in the current branch.
-if [[ $(git status --porcelain) ]]; then
-    echo "Stashing uncommitted changes in current branch..."
-    git stash
-    CURRENT_BRANCH_STASHED=true
-fi
-
-# Stash changes for the target branch.
+# Stash original, current branch
+ORIGIN_STASHED=false
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-git checkout "$1"
 if [[ $(git status --porcelain) ]]; then
-    echo "Stashing uncommitted changes on branch $1..."
-    git stash
-    TARGET_BRANCH_STASHED=true
+  if ! git stash; then
+    echo "Stash error for $CURRENT_BRANCH."
+    exit "$STASH_ORIG_ERR"
+  fi
+  ORIGIN_STASHED=true
 fi
-git checkout "$CURRENT_BRANCH"
 
-# Extract MAIN_DIRS from BRANCHES and set the FILE_PATH.
-for branch_item in "${BRANCHES[@]}"; do
-    case "$branch_item" in
-        main)
-            MAIN_DIRS=("development" "staging" "production")
-            ;;
-        code-*)
-            MAIN_DIRS=("${branch_item#code-}")
-            ;;
-    esac
-done
+# Checkout target branch
+if ! git checkout "$branch"; then
+  echo "Checkout error for $branch."
+  exit "$TARGET_CHECKOUT_ERR"
+fi
 
-case "$branch" in
-    main)
-        PS3='Which directory do you want to update in main? (or 4 to cancel): '
-        select dir in "${MAIN_DIRS[@]}" "cancel"; do
-            case $dir in
-                development|staging|production)
-                    break
-                    ;;
-                cancel)
-                    echo "User cancelled. Exiting."
-                    exit 5
-                    ;;
-            esac
-        done
-        FILE_PATH="$CURRENT_DIR/../../$dir/$PROJ_NAME/workflow.driver"
-        USER_INFO_STR=${USER_INFO["$branch"]}
-        USER_NAME=${USER_INFO_STR%% *}
-        USER_EMAIL=${USER_INFO_STR#* }
-        ;;
-    code-*)
-        dir="${branch#code-}"
-        FILE_PATH="$CURRENT_DIR/../../$dir/$PROJ_NAME/workflow.driver"
-        USER_INFO_STR=${USER_INFO["$branch"]}
-        USER_NAME=${USER_INFO_STR%% *}
-        USER_EMAIL=${USER_INFO_STR#* }
-        ;;
-esac
+# Stash target branch
+TARGET_STASHED=false
+if [[ $(git status --porcelain) ]]; then
+  if ! git stash; then
+    echo "Stash error for $branch."
+    exit "$STASH_TARG_ERR"
+  fi
+  TARGET_STASHED=true
+fi
 
-# Inform user about the operations.
-echo "Updating file: $FILE_PATH for branch: $branch"
+# Set workflow driver file
+DRIVER="$CUR_DIR/../../$env/$PROJ_NAME/workflow.driver"
+[ ! -f "$DRIVER" ] && touch "$DRIVER"
 
-# Configure git
-git config --replace-all user.name "$USER_NAME"
-git config --replace-all user.email "$USER_EMAIL"
-
-# Checkout the branch
-git fetch --all
-git checkout "$branch"
-git pull
-
-# Function to update workflow driver content
-function update_workflow_driver() {
-    {
-        echo "Push iteration: $1 of $2 to branch: $branch by:"
-        echo "Username: $USER_NAME Email: $USER_EMAIL"
-        echo "Automated push $1 of $2"
-        echo ""
-    } > "$FILE_PATH"
+# Function to update workflow.driver
+update_workflow_driver() {
+  echo "
+    Push iteration: $1 of $num_pushes
+    Commit Message: $2
+    Wait interval: $wait_duration
+    Target branch: $branch
+    Environment: $env
+    Driver: $DRIVER
+    Csproj: $CSPROJ
+    Username: $USER_NAME
+    Email: $USER_EMAIL" > "$DRIVER"
 }
 
-# Commit and push in a loop.
-for i in $(seq 1 "$num_commits"); do
-    update_workflow_driver "$i" "$num_commits"
-    git add "$FILE_PATH"
-    git commit -m "Running push $i of $num_commits at $wait_duration second intervals to $branch branch."
-    git push
+# Add, commit and push in a loop.
+for i in $(seq 1 "$num_pushes"); do
+  commit_msg="Automated push $i of $num_pushes to $branch ($env) by $USER_NAME."
+  update_workflow_driver "$i" "$commit_msg"
 
-    # Countdown timer
-    if [ "$i" -lt "$num_commits" ]; then
-        echo "Waiting for the next push..."
-        for j in $(seq "$wait_duration" -1 1); do
-            echo -ne "$j seconds remaining...\r"
-            sleep 1
-        done
-        echo
-    fi
+  if ! git add "$DRIVER"; then
+    echo "Add error."
+    exit "$ADD_ERR"
+  fi
+
+  if ! git commit -m "$commit_msg"; then
+    echo "Commit error for $branch push $i of $num_pushes."
+    exit "$COMMIT_ERR"
+  fi
+
+  if ! git push; then
+    echo "Push error."
+    exit "$PUSH_ERR"
+  fi
+
+  # Wait if required
+  [ "$i" -lt "$num_pushes" ] && sleep "$wait_duration"
 done
 
-# Return to main and reset user and dir.
-git config --replace-all user.name "$MAIN_USER"
-git config --replace-all user.email "$MAIN_EMAIL"
-git checkout main
-cd "$CURRENT_DIR"
-
-# Pop the stash for the target branch.
-if [[ $TARGET_BRANCH_STASHED == true ]]; then
-    git checkout "$1"
-    git stash pop
-    git checkout "$CURRENT_BRANCH"
+# Pop target branch
+if $TARGET_STASHED && ! git stash pop; then
+  echo "Stash pop error for $branch."
+  exit "$TARGET_POP_ERR"
 fi
 
-# Pop the stash for the current branch.
-if [[ $CURRENT_BRANCH_STASHED == true ]]; then
-    git stash pop
+# Switch to original user and branch
+git config user.name "$DEV_USER"
+git config user.email "$DEV_EMAIL"
+if ! git checkout "$CURRENT_BRANCH"; then
+  echo "Checkout error for $CURRENT_BRANCH."
+  exit "$ORIGIN_CHECKOUT_ERR"
 fi
+
+# Pop original branch
+if $ORIGIN_STASHED && ! git stash pop; then
+  echo "Stash pop error for $CURRENT_BRANCH."
+  exit "$ORIGIN_POP_ERR"
+fi
+
+# Exit successfully
+echo "Completed."
+exit "$SUCCESS"
