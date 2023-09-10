6c6d610000850fbd248505bf89e6fa7d9ef83bee
ScriptShifters <lodgings@pm.me>
2023-09-10 20:05:46 +0100

    Updating scripts

--- scripts/.devops/reset-all-branches-to-main.sh
+++ scripts/.devops/reset-all-branches-to-main.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# The script resets local main to remote main.
-# It then resets and updates the code-development, code-staging and code-production branches,
-# both locally and remotely.
-
-# It read a configuration file (.config)
-# to populate parameters such as branch names, user details, etc.
+# Script Description: Resets local main to remote main and updates the specific branches.
+# Reads a configuration file (.config) for branch names, user details, and more.
 
 # Exit codes
 SUCCESS=0
 USER_ABORT=60
 USAGE_ERR=61
-RM_ERR=62
-CP_ERR=63
+CP_ERR=62
+RM_ERR=63
 NAV_ERR=64
 MKDIR_ERR=65
+CD_ERR=66
 
 # Git exit codes
 GIT_CONFIG_ERR=120
 GIT_MAIN_ERR=121
-GIT_BRANCH_DEL_ERR=122
-GIT_BRANCH_CREATE_ERR=123
+GIT_CREATE_ERR=122
+GIT_DELETE_ERR=123
 GIT_CHECKOUT_ERR=124
 GIT_STASH_ERR=125
 GIT_PUSH_ERR=126
+GIT_ADD_ERR=127
+GIT_COMMIT_ERR=128
 
-# Set constants from .config
+# Load constants from .config
 mapfile -t CONFIG_VALUES < <(grep -vE '^#|^[[:space:]]*$' .config)
 DEVOPS_USER="${CONFIG_VALUES[0]}"
 DEVOPS_EMAIL="${CONFIG_VALUES[1]}"
@@ -33,118 +32,84 @@ EXPECTED_DIR="${CONFIG_VALUES[3]}"
 BRANCHES=("${CONFIG_VALUES[4]}" "${CONFIG_VALUES[5]}" "${CONFIG_VALUES[6]}" "${CONFIG_VALUES[7]}")
 CUR_DIR=$(pwd)
 
-# Warning message
-WARNING=$(cat << EOM
-
-WARNING: You are about to execute $0
-
-This script replaces local ${BRANCHES[3]} with remote ${BRANCHES[3]},
-and then resets the ${BRANCHES[0]}, ${BRANCHES[1]} and ${BRANCHES[2]}
-branches both locally and remotely.
-
-The script reads parameters from:
-$CUR_DIR/.config
-
+# Display Warning
+cat << EOM
+WARNING:
+Executing $0 will replace the local ${BRANCHES[3]} with remote ${BRANCHES[3]}.
+It will reset the ${BRANCHES[0]}, ${BRANCHES[1]}, and ${BRANCHES[2]} branches both locally and remotely.
+Parameters are read from: $CUR_DIR/.config
+CAUTION: This can lead to loss of unsaved work. Consider backups before executing.
 USAGE: $0
-
-CAUTION: This script could result in losing unsaved work.
-         Consider making a backup of all branches before execution.
-
 EOM
-)
 
-# Logging function
-log_entry() {
-  local message="$1"
-  echo "$(date +'%Y-%m-%d %H:%M:%S') - $message"
-}
+# User confirmation
+read -r -p "CONTINUE? [yes/no]: " response
+[[ ! "yes" =~ ^$response$ ]] && log_entry "Aborted." && exit "$USER_ABORT"
 
-# Issue warning and parse user response
-echo && echo "$WARNING"
-echo && read -r -p "CONTINUE ??? [yes/no] " response
-responses=("y" "Y" "yes" "YES" "Yes")
-[[ ! "${responses[*]}" =~ $response ]] && log_entry "Aborted." && exit "$USER_ABORT"
-echo
+# Ensure we're in the correct directory
+[ "$(pwd)" != *"$EXPECTED_DIR" ] && log_entry "Please run from $EXPECTED_DIR." && exit "$USAGE_ERR"
 
-# Ensure correct directory
-if [[ "$(pwd)" != *"$EXPECTED_DIR" ]]; then
-  log_entry "Error: Must run from $EXPECTED_DIR."
-  exit "$USAGE_ERR"
-fi
-
-# Navigate to root directory
-cd ../.. || { log_entry "Error: Could not navigate to root directory."; exit "$NAV_ERR"; }
-
-# ===============================================
-# Main Script Logic
-# ===============================================
+# Move to root directory
+cd ../.. || { log_entry "Navigation error."; exit "$NAV_ERR"; }
 
 # Set Git User
-git config user.name "$DEVOPS_USER" || { log_entry "Git config user.name failed."; exit "$GIT_CONFIG_ERR"; }
-git config user.email "$DEVOPS_EMAIL" || { log_entry "Git config user.email failed."; exit "$GIT_CONFIG_ERR"; }
+git config user.name "$DEVOPS_USER" || { log_entry "Git config user.name error."; exit "$GIT_CONFIG_ERR"; }
+git config user.email "$DEVOPS_EMAIL" || { log_entry "Git config user.email error."; exit "$GIT_CONFIG_ERR"; }
 
-# Ensure main branch
-git checkout "${BRANCHES[3]}" || { log_entry "Git checkout main failed."; exit "$GIT_CHECKOUT_ERR"; }
-git stash || { log_entry "Git stash failed for main."; exit "$GIT_STASH_ERR"; } # Stash changes to prevent issues
+# Ensure we're on the main branch
+git checkout "${BRANCHES[3]}" || { log_entry "Checkout main error."; exit "$GIT_CHECKOUT_ERR"; }
+git stash || { log_entry "Stash error on main."; exit "$GIT_STASH_ERR"; }
 
-# Reset Main
-git fetch origin || { log_entry "Git fetch failed."; exit "$GIT_MAIN_ERR"; }
-git reset --hard origin/main || { log_entry "Git reset main failed."; exit "$GIT_MAIN_ERR"; }
+# Reset main branch to mirror remote
+git fetch origin || { log_entry "Git fetch error."; exit "$GIT_MAIN_ERR"; }
+git reset --hard origin/main || { log_entry "Git reset error."; exit "$GIT_MAIN_ERR"; }
 
-# Branch Operations
+# Recreate each code- branch
 for branch in "${BRANCHES[@]:0:3}"; do
 
-  # Check if the branch exists locally, stash changes, switch to main, delete branch
+  # Checkout, stash and delete local branch
   if git show-ref --verify --quiet "refs/heads/$branch"; then
-    git checkout "$branch" || { log_entry "Git checkout $branch failed."; exit "$GIT_CHECKOUT_ERR"; }
-    git stash || { log_entry "Git stash failed for $branch."; exit "$GIT_STASH_ERR"; }
-    git checkout "${BRANCHES[3]}" || { log_entry "Git checkout main failed."; exit "$GIT_CHECKOUT_ERR"; } # Switch to main branch
-    git branch -D "$branch" || { log_entry "Git delete $branch failed."; exit "$GIT_BRANCH_DEL_ERR"; }
+    git checkout "$branch"
+    git stash
+    git checkout "${BRANCHES[3]}"
+    git branch -D "$branch"
   fi
 
-  # Check if the branch exists remotely and delete it
-  if git ls-remote --heads origin "$branch" | grep -sw "$branch" >/dev/null; then
-    git push origin --delete "$branch" || { log_entry "Git delete remote $branch failed."; exit "$GIT_BRANCH_DEL_ERR"; }
-  fi
+  # Delete remote branch if it exists
+  git ls-remote --heads origin "$branch" | grep -sw "$branch" >/dev/null && git push origin --delete "$branch"
 
-  # Create new code- branch from main
-  git checkout -b "$branch" || { log_entry "Git checkout new $branch failed."; exit "$GIT_BRANCH_CREATE_ERR"; }
+  # Create a new branch from main
+  git checkout -b "$branch" || { log_entry "Error creating branch $branch."; exit "$GIT_CREATE_ERR"; }
 
-  # Remove all files in the branch, including hidden ones
-  rm -rf ./* ./.[^.]* || { log_entry "Directory cleanup failed."; exit "$RM_ERR"; }
+  # Create toolbox directory
+  mkdir -p toolbox || { log_entry "Error creating toolbox."; exit "$MKDIR_ERR"; }
 
-  # Copy only required directories to branch
+  # Copy required scripts to toolbox dir
   env_name="${branch#code-}"
+  cp -r "scripts/$env_name/*" toolbox/ || { log_entry "Error copying scripts to toolbox."; exit "$CP_ERR"; }
+
+  # Cleanup directories based on branch
   case "$branch" in
-    "${BRANCHES[0]}")
-      cp -r "./development" . || { log_entry "Copy operation for development failed."; exit "$CP_ERR"; }
-      ;;
-    "${BRANCHES[1]}")
-      cp -r "./development" . || { log_entry "Copy operation for development failed."; exit "$CP_ERR"; }
-      cp -r "./staging" . || { log_entry "Copy operation for staging failed."; exit "$CP_ERR"; }
-      ;;
-    "${BRANCHES[2]}")
-      cp -r "./staging" . || { log_entry "Copy operation for staging failed."; exit "$CP_ERR"; }
-      cp -r "./production" . || { log_entry "Copy operation for production failed."; exit "$CP_ERR"; }
-      ;;
+    "${BRANCHES[0]}") rm -rf staging production || { log_entry "Error removing directories from ${BRANCHES[0]}."; exit "$RM_ERR"; } ;;
+    "${BRANCHES[1]}") rm -rf production || { log_entry "Error removing directories from ${BRANCHES[1]}."; exit "$RM_ERR"; } ;;
+    "${BRANCHES[2]}") rm -rf development || { log_entry "Error removing directories from ${BRANCHES[2]}."; exit "$RM_ERR"; } ;;
   esac
 
-  # Copy required scripts to code- toolbox dir
-  mkdir -p toolbox || { log_entry "Toolbox directory creation failed."; exit "$MKDIR_ERR"; }
-  cp -r "./scripts/$env_name/*" toolbox/ || { log_entry "Copy operation for toolbox failed."; exit "$CP_ERR"; }
+  # Remove scripts
+  rm -rf scripts || { log_entry "Error removing scripts."; exit "$RM_ERR"; }
 
-  # Add, commit and push code- branch to remote
-  git add . || { log_entry "Git add failed for $branch."; exit "$GIT_MAIN_ERR"; }
-  git commit -m "Updated $branch from main"
-  git push -u origin "$branch" || { log_entry "Git push to $branch failed."; exit "$GIT_PUSH_ERR"; }
+  # Add, commit, and push to remote
+  git add . || { log_entry "Git add error."; exit "$GIT_ADD_ERR"; }
+  git commit -m "Updated $branch from main" || { log_entry "Git commit error."; exit "$GIT_COMMIT_ERR"; }
+  git push -u origin "$branch" || { log_entry "Git push error."; exit "$GIT_PUSH_ERR"; }
 
 done
 
-# Cleanup
-git checkout main || { log_entry "Git checkout main failed."; exit "$GIT_CHECKOUT_ERR"; }
-git stash drop || { log_entry "Git stash drop failed."; exit "$GIT_STASH_ERR"; }
-CD "$CUR_DIR" || { log_entry "Error: Could not navigate to $CUR_DIR."; exit "$NAV_ERR"; }
+# Final steps
+git checkout "${BRANCHES[3]}" || { log_entry "Checkout main error."; exit "$GIT_CHECKOUT_ERR"; }
+git stash drop || { log_entry "Stash drop error."; exit "$GIT_STASH_ERR"; }
+cd "$CUR_DIR" || { log_entry "Error navigating back to $CUR_DIR."; exit "$CD_ERR"; }
 
-# Finish
-log_entry "$0 completed successfully."
+# Completion message
+log_entry "$0 finished."
 exit "$SUCCESS"
