046487a9b14f160c805b7744d13332eb6bd89626
CodeApprover <pucfada@pm.me>
2023-11-18 13:41:45 +0000

    resetting dev folder

--- scripts/.devops/reset-all-branches-using-remote-main.sh
+++ scripts/.devops/reset-all-branches-using-remote-main.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Script Description: Resets local main to remote main and updates specific branches.
-
-# Check if JQ is installed
+# Check dependencies
 if ! command -v jq &> /dev/null; then
   echo "jq is not installed."
   exit 1
@@ -16,85 +14,85 @@ fi
 
 # Load the JSON config
 JSON_CONFIG=$(cat config.json)
-
-# Ensure JSON is valid
 if ! jq empty <<< "$JSON_CONFIG" &>/dev/null; then
   echo "Invalid JSON in config.json."
   exit 1
 fi
 
-# Extract constants from json
+# Extract constants from JSON
 DEVOPS_USER=$(echo "$JSON_CONFIG" | jq -r '.DevOpsUser.name')
 DEVOPS_EMAIL=$(echo "$JSON_CONFIG" | jq -r '.DevOpsUser.email')
 EXPECTED_DIR=$(echo "$JSON_CONFIG" | jq -r '.ProjectConfig.dir')
 
-# Extract branch names from json keys
+# Extract branch names from JSON keys
 mapfile -t BRANCHES < <(echo "$JSON_CONFIG" | jq -r '.Users | keys[]' | tr -d '\r')
 
-# Ensure correct directory
-CUR_DIR="$(dirname "$0")"
-cd "$CUR_DIR" || exit 1
+# Change to the expected directory
+cd "$EXPECTED_DIR" || { echo "Failed to navigate to expected directory: $EXPECTED_DIR"; exit 1; }
 
 # Set Git User
 git config user.name "$DEVOPS_USER"
 git config user.email "$DEVOPS_EMAIL"
 
-# Checkout main branch and update it
+# Update main branch
 git checkout main
 git pull origin main
-
-# Reset local main branch to mirror remote main
 git fetch origin
 git reset --hard origin/main
 
-# Process each code- branch as required
+# Process each code- branch
 for branch in "${BRANCHES[@]}"; do
-  # Specify working branch
-  echo "Working on branch: $branch"
+  echo "Processing branch: $branch"
 
   # Delete local branch if exists
-  git branch | grep -q "$branch" && git branch -D "$branch"
-
-  # Delete remote branch if exists
-  git ls-remote --heads origin "$branch" | grep -q "$branch" && git push origin --delete "$branch"
-
-  # Create a new branch from main and switch to it
-  git checkout -b "$branch"
-
-  # Create toolbox directory and copy scripts
-  mkdir -p toolbox
-  script_dir="scripts/${branch#code-}"
-  if [[ -d "$script_dir" ]]; then
-    cp -r "$script_dir/"* toolbox/
+  if git rev-parse --verify "$branch" > /dev/null 2>&1; then
+    git branch -D "$branch"
   fi
 
-  # Remove scripts directory
+  # Delete remote branch if exists
+  if git ls-remote --heads origin "$branch" | grep -q "$branch"; then
+    git push origin --delete "$branch"
+  fi
+
+  # Create new branch from main
+  git checkout -b "$branch"
+  mkdir -p toolbox
+  script_dir="scripts/${branch#code-}"
+  
+  # Copy scripts and remove scripts directory
+  if [[ -d "$script_dir" ]]; then
+    cp -r "$script_dir/"* toolbox/ || { echo "Failed to copy from $script_dir to toolbox"; exit 1; }
+    git add toolbox/*
+  else
+    echo "Directory $script_dir does not exist or is empty."
+  fi
   rm -rf scripts
 
-  # Perform branch-specific directory cleanup
+  # Branch-specific directory cleanup
   case "$branch" in
     "${BRANCHES[0]}") # code-development
-      # Retain only the development folder
       rm -rf staging production
       ;;
     "${BRANCHES[1]}") # code-production
-      # Retain only the staging folder
       rm -rf development
       ;;
     "${BRANCHES[2]}") # code-staging
-      # Retain development and staging folders, remove production
       rm -rf production
       ;;
   esac
 
-  # Add changes, commit, and push new branch to remote
-  git add .
-  git commit -m "Updated $branch from main"
-  git push -u origin "$branch"
-done
+  # Commit and push changes
+  if ! git diff --quiet; then
+    git add .
+    git commit -m "Updated $branch from main"
+    git push -u origin "$branch"
+  else
+    echo "No changes to commit for $branch."
+  fi
 
-# Return to the main branch
-git checkout main
+  # Return to main branch
+  git checkout main
+done
 
 # Drop all stashes
 git stash clear
