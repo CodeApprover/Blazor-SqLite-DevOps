64b6053a9695195c885099d45710f47f73c366b2
CodeApprover <pucfada@pm.me>
2023-11-19 10:20:54 +0000

    updating scripts

--- scripts/.devops/auto-push-workflow.sh
+++ scripts/.devops/auto-push-workflow.sh
@@ -5,14 +5,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Function to generate a random 5-character string
-generate_random_string() {
-    cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1
-}
-
 # Main script execution
-BRANCHES=("code-development" "code-staging" "code-production")
-branch=$1
+branch="code-development"
+
+# Set Git user to code-backups
+git config user.name "code-backups" || { echo "Error setting Git user to code-backups."; exit 1; }
 
 # Remove "code-" prefix from branch name
 environment=${branch#"code-"}
@@ -23,15 +20,19 @@ workflow_driver="../../$environment/Blazor-SqLite-Golf-Club/workflow.driver"
 # Check if the workflow.driver file exists
 if [[ ! -f "$workflow_driver" ]]; then
     echo "workflow.driver file not found in $workflow_driver."
-    exit 4
+    exit 2
 fi
 
 # Git operations
-git checkout "$branch" || { echo "Git checkout error on $branch."; exit 1; }
+git checkout "$branch" || { echo "Git checkout error on $branch."; exit 3; }
 commit_msg="$(date +'%Y-%m-%d %H:%M:%S') - Automated push"
 echo "$commit_msg" > "$workflow_driver"
-git add "$workflow_driver" || { echo "Git add error."; exit 1; }
-git commit -m "$commit_msg" || { echo "Git commit error."; exit 2; }
-git push || { echo "Git push error."; exit 1; }
-git checkout main || { echo "Git checkout error returning to main."; exit 3; }
-echo "$commit_msg"
+git add "$workflow_driver" || { echo "Git add error."; exit 4; }
+git commit -m "$commit_msg" || { echo "Git commit error."; exit 5; }
+git push || { echo "Git push error."; exit 6; }
+
+# Switch back to the main branch
+git checkout main || { echo "Git checkout error returning to main."; exit 7; }
+
+# Switch Git user back to codeapprover
+git config user.name "codeapprover" || { echo "Error setting Git user to codeapprover."; exit 8; }
